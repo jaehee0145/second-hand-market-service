e667b2c836387040700f092aba6301c8ebfbfdd4
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 14:04:54 +0900

    feat: @Valid 어노테이션을 사용해서 요청 객체에 대한 validation과 세부 메세지 설정

--- src/main/java/com/itembay/api/ItemApi.java
+++ src/main/java/com/itembay/api/ItemApi.java
@@ -3,6 +3,7 @@ package com.itembay.api;
 import com.itembay.domain.Item;
 import com.itembay.dto.ItemRegisterReqData;
 import com.itembay.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class ItemApi {
     private final ItemService itemService;
 
     @PostMapping("/api/items")
-    public ResponseEntity registerItem(@RequestBody ItemRegisterReqData req) {
+    public ResponseEntity registerItem(@Valid @RequestBody ItemRegisterReqData req) {
         Item newItem = itemService.registerItem(req);
         // TODO. Jaehee Park 26.01.09 엔티티 대신 DTO 반환하도록 수정 예정
         return ResponseEntity.status(201).body(newItem);
