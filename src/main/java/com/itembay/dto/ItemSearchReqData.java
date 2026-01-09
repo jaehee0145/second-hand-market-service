ceec650e88e5f5ca3dc601676563bb7278f767ce
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 17:25:36 +0900

    feat: 검색 필드 누락된 경우에 메세지 표시하도록 어노테이션 추가

--- src/main/java/com/itembay/dto/ItemSearchReqData.java
+++ src/main/java/com/itembay/dto/ItemSearchReqData.java
@@ -2,15 +2,24 @@ package com.itembay.dto;
 
 import com.itembay.domain.enums.ItemSortType;
 import com.itembay.domain.enums.ItemType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Builder
 public record ItemSearchReqData(
+
+        @NotBlank(message = "상품명은 필수입니다.")
         String title,
+
+        @NotBlank(message = "서버 이름은 필수입니다.")
         String server,
+
+        @NotNull(message = "상품 종류는 필수입니다.")
         ItemType itemType,
+
         BigDecimal minPrice,
         BigDecimal maxPrice,
         ItemSortType itemSortType,
