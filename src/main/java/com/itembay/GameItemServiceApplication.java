29a68d2ab142b3c7647b837130c0cc023a438be8
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 10:55:18 +0900

    feat: application 클래스 추가

---
+++ src/main/java/com/itembay/GameItemServiceApplication.java
@@ -0,0 +1,12 @@
+package com.itembay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameItemServiceApplication.class, args);
+    }
+}
