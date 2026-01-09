6a6706e8a9f1bbde23022260e03ad3dca478b800
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 11:01:38 +0900

    feat: ItemType enum 클래스 추가

---
+++ src/main/java/com/itembay/domain/enums/ItemType.java
@@ -0,0 +1,19 @@
+package com.itembay.domain.enums;
+
+/**
+ * Created by Jaehee Park on 2026.01.09
+ */
+public enum ItemType {
+    GAME_MONEY("enum_game_money", "게임 머니"),
+    ITEM("enum_item", "아이템"),
+    ACCOUNT("enum_account", "계정"),
+    ETC("enum_etc", "기타");
+
+    String key;
+    String description;
+
+    ItemType(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+}
