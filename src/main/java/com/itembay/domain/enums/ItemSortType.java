0820b3327941953d564ccf9a9c18117f13723872
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 16:17:38 +0900

    feat: 아이템 정렬 타입 enum 클래스 추가

---
+++ src/main/java/com/itembay/domain/enums/ItemSortType.java
@@ -0,0 +1,16 @@
+package com.itembay.domain.enums;
+
+public enum ItemSortType {
+    PRICE_ASC("enum_price_asc", "가격 오름차순"),
+    PRICE_DESC("enum_price_desc", "가격 내림차순"),
+    CREATED_ASC("enum_created_asc", "생성일 오름차순"),
+    CREATED_DESC("enum_created_desc", "생성일 내림차순");
+
+    String key;
+    String description;
+
+    ItemSortType(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+}
