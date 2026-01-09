e3d206d27c33dffba2f5cd5d0ede5453143fcdcb
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 13:35:08 +0900

    test: 아이템 등록 실패하는 경우 400 응답 발생하는 테스트 추가

--- src/test/java/com/itembay/api/ItemApiTest.java
+++ src/test/java/com/itembay/api/ItemApiTest.java
@@ -15,8 +15,7 @@ import java.math.BigDecimal;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -55,6 +54,62 @@ public class ItemApiTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.title").value("다야 팝니다 필요하신만큼 신청해주세요"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("아이템 등록에 실패한다. - 가격 오류인 경우 400 응답")
+    public void register_item_failed_price_error() throws Exception {
+
+        // given
+        String server = "라엘08";
+        String sellerName = "아리";
+        ItemType itemType = ItemType.GAME_MONEY;
+        String title = "다야 팝니다 필요하신만큼 신청해주세요";
+        BigDecimal price = new BigDecimal(-25470);
+        int quantity = 3000;
+
+        ItemRegisterReqData request = ItemRegisterReqData.builder()
+                .server(server)
+                .sellerName(sellerName)
+                .itemType(itemType)
+                .title(title)
+                .price(price)
+                .quantity(quantity)
+                .build();
+
+        mockMvc.perform(post("/api/items")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("거래 가격은 필수이고 0보다 커야 합니다."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("아이템 등록에 실패한다. - 서버 이름 누락인 경우 400 응답")
+    public void register_item_failed_server_name_missing() throws Exception {
+
+        // given
+        String sellerName = "아리";
+        ItemType itemType = ItemType.GAME_MONEY;
+        String title = "다야 팝니다 필요하신만큼 신청해주세요";
+        BigDecimal price = new BigDecimal(25470);
+        int quantity = 3000;
+
+        ItemRegisterReqData request = ItemRegisterReqData.builder()
+                .sellerName(sellerName)
+                .itemType(itemType)
+                .title(title)
+                .price(price)
+                .quantity(quantity)
+                .build();
+
+        mockMvc.perform(post("/api/items")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("서버 이름은 필수입니다."))
                 .andDo(print());
     }
 }
