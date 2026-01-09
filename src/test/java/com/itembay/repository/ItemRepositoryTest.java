c75023af1d530a7dc402ceedbefc1b37f124e659
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 11:39:06 +0900

    test: 아이템 등록 성공 테스트 추가

---
+++ src/test/java/com/itembay/repository/ItemRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.itembay.repository;
+
+import com.itembay.domain.Item;
+import com.itembay.domain.enums.ItemType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@SpringBootTest
+@DisplayName("Item Repository Test")
+public class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("아이템 등록에 성공한다.")
+    public void register_item() {
+
+        // given
+        String server = "라엘08";
+        String sellerName = "아리";
+        ItemType itemType = ItemType.GAME_MONEY;
+        String title = "다야 팝니다 필요하신만큼 신청해주세요";
+        BigDecimal price = new BigDecimal(25470);
+        int quantity = 3000;
+
+        Item newItem = Item.builder()
+                .server(server)
+                .sellerName(sellerName)
+                .itemType(itemType)
+                .title(title)
+                .price(price)
+                .quantity(quantity)
+                .build();
+
+        itemRepository.save(newItem);
+
+        // when
+        List<Item> itemList = itemRepository.findAll();
+        Item firstItem = itemList.getFirst();
+
+        //then
+        assert firstItem.getServer().equals(server);
+    }
+}
