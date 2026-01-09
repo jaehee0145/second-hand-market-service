ad83cf150989e977f1c8f14d7a94710f52f5250e
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 13:34:24 +0900

    feat: 필드 validation으로 IllegalArgumentException이 발생한 경우 400 BadRequest를 리턴하도록 Exception Handler 추가

---
+++ src/main/java/com/itembay/error/ApiExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.itembay.error;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
