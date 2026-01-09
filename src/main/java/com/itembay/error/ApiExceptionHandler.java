d99326a7ee3a5e8e323bd8156909cd93d12e0783
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 18:05:19 +0900

    feat: 별도 처리되지 못한 모든 예외를 처리하는 handler 추가

--- src/main/java/com/itembay/error/ApiExceptionHandler.java
+++ src/main/java/com/itembay/error/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.itembay.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +20,15 @@ public class ApiExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         return ResponseEntity.badRequest().body(Objects.requireNonNull(e.getFieldError()).getDefaultMessage());
+    }
+
+    /**
+     * 캐치되지 못한 예외를 처리하기 위한 Handler
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<String> handleException(Exception e) {
+        // 개발자가 예상하지 못한 에러는 log로 남기고 사용자에게는 오류 메세지를 보여준다.
+        log.error("handleException", e);
+        return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
     }
 }
