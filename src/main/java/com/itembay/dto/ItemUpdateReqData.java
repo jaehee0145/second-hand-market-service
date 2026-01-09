9110befd88ad232a89661a4fe9223a90fb007137
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 22:49:35 +0900

    feat: 테스트 코드에서 아이템 수정 DTO를 사용하기 위해 @Builder 추가

--- src/main/java/com/itembay/dto/ItemUpdateReqData.java
+++ src/main/java/com/itembay/dto/ItemUpdateReqData.java
@@ -4,9 +4,11 @@ import com.itembay.domain.enums.ItemType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@Builder
 public record ItemUpdateReqData(
 
         Long id,
