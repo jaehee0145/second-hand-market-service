61cd11e259aaf1603b58c6d5ff22a5bd6ffc0421
Jaehee <jaeheepark0145@gmail.com>
2026-01-10 05:08:01 +0900

    feat: 아이템 조회시 필수 필드로 설정한 부분 삭제

--- src/main/java/com/itembay/dto/ItemSearchReqData.java
+++ src/main/java/com/itembay/dto/ItemSearchReqData.java
@@ -2,8 +2,6 @@ package com.itembay.dto;
 
 import com.itembay.domain.enums.ItemSortType;
 import com.itembay.domain.enums.ItemType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -11,19 +9,20 @@ import java.math.BigDecimal;
 @Builder
 public record ItemSearchReqData(
 
-        @NotBlank(message = "상품명은 필수입니다.")
         String title,
-
-        @NotBlank(message = "서버 이름은 필수입니다.")
         String server,
-
-        @NotNull(message = "상품 종류는 필수입니다.")
         ItemType itemType,
-
         BigDecimal minPrice,
         BigDecimal maxPrice,
         ItemSortType itemSortType,
-        int page,
-        int size
+        Integer page,
+        Integer size
 ) {
+    public int getPage() {
+        return page == null ? 0 : page;
+    }
+
+    public int getSize() {
+        return size == null ? 5 : size;
+    }
 }
