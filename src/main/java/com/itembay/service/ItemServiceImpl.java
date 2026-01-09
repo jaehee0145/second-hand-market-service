d851f6eab1aefc25f391c2c98cdf6b6ef26fea51
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 12:50:02 +0900

    feat: 아이템 등록 요청 필드에 대한 validation 추가

--- src/main/java/com/itembay/service/ItemServiceImpl.java
+++ src/main/java/com/itembay/service/ItemServiceImpl.java
@@ -7,6 +7,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -16,6 +18,30 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public Item registerItem(ItemRegisterReqData req) {
+
+        if (req.server() == null || req.server().isBlank()) {
+            throw new IllegalArgumentException("서버 이름은 필수입니다.");
+        }
+
+        if (req.sellerName() == null || req.sellerName().isBlank()) {
+            throw new IllegalArgumentException("판매자 닉네임은 필수입니다.");
+        }
+
+        if (req.itemType() == null) {
+            throw new IllegalArgumentException("상품 종류는 필수입니다.");
+        }
+
+        if (req.title() == null || req.title().isBlank()) {
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        }
+
+        if (req.price() == null || req.price().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("거래 가격은 필수이고 0보다 커야 합니다.");
+        }
+
+        if (req.quantity() < 0) {
+            throw new IllegalArgumentException("판매 수량은 0보다 커야 합니다.");
+        }
 
         Item newItem = Item.builder()
                 .server(req.server())
