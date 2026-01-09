9a7364744f928e2e3f02ae4497a7c69a77bad8dc
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 17:10:24 +0900

    feat: 아이템 조회 API 추가

--- src/main/java/com/itembay/api/ItemApi.java
+++ src/main/java/com/itembay/api/ItemApi.java
@@ -2,10 +2,16 @@ package com.itembay.api;
 
 import com.itembay.domain.Item;
 import com.itembay.dto.ItemRegisterReqData;
+import com.itembay.dto.ItemResponse;
+import com.itembay.dto.ItemSearchReqData;
+import com.itembay.dto.PageResponse;
 import com.itembay.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +28,12 @@ public class ItemApi {
         // TODO. Jaehee Park 26.01.09 엔티티 대신 DTO 반환하도록 수정 예정
         return ResponseEntity.status(201).body(newItem);
     }
+
+    @GetMapping("/api/items")
+    public ResponseEntity<PageResponse<ItemResponse>> searchItem(@Valid @ModelAttribute ItemSearchReqData req) {
+        Page<Item> itemPage = itemService.searchItem(req);
+        Page<ItemResponse> responsePage = itemPage.map(ItemResponse::from);
+        return ResponseEntity.ok(PageResponse.of(responsePage));
+    }
+
 }
