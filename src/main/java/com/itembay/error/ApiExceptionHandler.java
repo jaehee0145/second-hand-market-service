92c111463f3a63fd1ca86882727b95ff564b9af3
Jaehee <jaeheepark0145@gmail.com>
2026-01-09 14:05:44 +0900

    feat: @Valid 어노테이션을 사용했을때 발생하는 MethodArgumentNotValidationException을 400 Bad Request로 처리하기 위해 Exception handler 설정

--- src/main/java/com/itembay/error/ApiExceptionHandler.java
+++ src/main/java/com/itembay/error/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.itembay.error;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +11,10 @@ public class ApiExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(e.getFieldError().getDefaultMessage());
     }
 }
